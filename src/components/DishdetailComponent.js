d9faf651aa974bddfbacad27cf3863c28e7842ba
neeravp <neerav.patel@loblaw.ca>
2022-11-08 00:46:49 -0500

    Week 1 Submission

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { Card, CardBody, CardImg, CardText, CardTitle } from 'reactstrap';
+import Moment from 'react-moment';
+
+class DishDetail extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            this.props.selectedDish != null ?
+                <div className='row'>
+                    <Card className='col-12 col-md-5 m-1'>
+                        <CardImg object src={this.props.selectedDish.image} alt={this.props.selectedDish.name} />
+                        <CardBody>
+                            <CardTitle heading>{this.props.selectedDish.name}</CardTitle>
+                            <CardText>{this.props.selectedDish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                    <Card className='col-12 col-md-5 m-1'>
+                        <CardBody>
+                            <CardTitle heading>Comments</CardTitle>
+                            {this.props.selectedDish.comments.map(comment => {
+                                return (
+                                    <CardText>
+                                        {comment.comment}{"\n"}<br></br>-- {comment.author},
+                                        <Moment format="MMM D, YYYY" withTitle>{comment.date}</Moment>
+                                    </CardText>
+                                )
+                            })}
+                        </CardBody>
+                    </Card>
+                </div> : <div></div>
+        );
+    }
+}
+
+export default DishDetail;
---
+++ src/components/MenuComponent.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import DishDetail from './DishdetailComponent';
+
+class Menu extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedDish: null
+        }
+    }
+
+    onDishSelect(dish) {
+        this.setState({
+            selectedDish: dish
+        })
+    }
+
+    render() {
+        const menu = this.props.dishes.map(dish => {
+            return (
+                <div key={dish.id} className='col-12 col-md-5 m-1' >
+                    <Card onClick={() => this.onDishSelect(dish)}>
+                        <CardImg width="100%" object src={dish.image} alt={dish.name} />
+                        <CardImgOverlay>
+                            <CardTitle heading className='m-4'>{dish.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
+                </div>
+            );
+        });
+
+        return (
+            <div>
+                <div className='container'>
+                    <div className='row'>{menu}</div>
+                    <DishDetail className='row' selectedDish={this.state.selectedDish} />
+                </div>
+            </div>
+        );
+    }
+}
+export default Menu;
