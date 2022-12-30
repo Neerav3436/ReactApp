7f26731044efc32342f5f15eed3f29dc66080143
neeravp <neerav.patel@loblaw.ca>
2022-12-30 17:13:45 -0500

    Pending redux changes from video 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -7,6 +7,7 @@ import Home from './HomeComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Contact from './ContactComponent';
+import { addComment } from '../redux/ActionCreators';
 
 
 
@@ -18,6 +19,10 @@ const mapStateToProps = state => {
         leaders: state.leaders
     }
 }
+
+const mapDispatchToProps = (dispatch) => ({
+    addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment))
+})
 
 class Main extends Component {
     constructor(props) {
@@ -38,6 +43,7 @@ class Main extends Component {
             return (
                 <DishDetail selectedDish={this.props.dishes.filter(dish => dish.id === parseInt(match.params.dishId, 10))[0]}
                     comments={this.props.comments.filter(comment => comment.dishId === parseInt(match.params.dishId, 10))}
+                    addComment={this.props.addComment}
                 />
             );
         }
@@ -57,4 +63,4 @@ class Main extends Component {
     }
 }
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
